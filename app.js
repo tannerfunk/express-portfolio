4dee99de2c0d7cd8724bd5e481d0a477cde95fcd
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-07-27 16:07:05 -0600

    build entire app w/o error handling

--- app.js
+++ app.js
@@ -2,29 +2,42 @@
 const express = require('express');
 const path = require('path');
 
-const data = require("data.json");
+const data = require("./data.json");
+
+const projects = data.projects
 
 //const { projects } = require('data.json');
 //const { application } = require('express');
 
 const app = express();
-
+const heading = "Tanner Funk";
 
 app.set('view engine', 'pug');
 
 app.use('/static', express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { projects });
 });
 
 app.get('/about', (req, res) => {
-    res.render('/about');
+    res.render('about');
 });
 
-/*app.get('/project', (req, res) => {
-    res.render(`/project/${project.id}`);
-})*/
+app.get('/project/:id', (req, res) => {
+    const id = req.params.id;
+    const name = projects[id].project_name;
+    const desc = projects[id].description;
+    const tech = projects[id].technologies;
+    const link = projects[id].live_link;
+    const github = projects[id].github_link;
+    const images = projects[id].image_urls;
+
+    const templateData = {id, name, desc, tech, link, github, images};
+
+
+    res.render(`project`, {templateData});
+})
 
 
 app.listen(3000, () => {
